6ac2ed12f5f0b988134cf8ab8df2d725ad81ea09
Fawwaz Naabigh <fnaabigh@gmail.com>
2022-09-01 21:20:32 +0700

    restyle navbar

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -35,98 +35,115 @@ class _NavBarState extends State<NavBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: CircleAvatar(
-                  radius: 25,
-                  backgroundColor: Colors.white,
-                  child: IconButton(
-                      onPressed: null,
-                      icon: Icon(
-                        Icons.home,
-                        color: Colors.black,
-                      ))),
-              label: '',
-              activeIcon: CircleAvatar(
-                  radius: 25,
-                  backgroundColor: Colors.blue,
-                  child: IconButton(
-                      onPressed: null,
-                      icon: Icon(
-                        Icons.home,
-                        color: Colors.white,
-                      )))),
-          BottomNavigationBarItem(
-              icon: CircleAvatar(
-                  radius: 25,
-                  backgroundColor: Colors.white,
-                  child: IconButton(
-                      onPressed: null,
-                      icon: Icon(
-                        Icons.map,
-                        color: Colors.black,
-                      ))),
-              label: '',
-              activeIcon: CircleAvatar(
-                  radius: 25,
-                  backgroundColor: Colors.blue,
-                  child: IconButton(
-                      onPressed: null,
-                      icon: Icon(
-                        Icons.map,
-                        color: Colors.white,
-                      )))),
-          BottomNavigationBarItem(
-              icon: CircleAvatar(
-                  radius: 25,
-                  backgroundColor: Colors.white,
-                  child: IconButton(
-                      onPressed: null,
-                      icon: Icon(
-                        Icons.location_searching,
-                        color: Colors.black,
-                      ))),
-              label: '',
-              activeIcon: CircleAvatar(
-                  radius: 25,
-                  backgroundColor: Colors.blue,
-                  child: IconButton(
-                      onPressed: null,
-                      icon: Icon(
-                        Icons.location_searching,
-                        color: Colors.white,
-                      )))),
-          BottomNavigationBarItem(
-              icon: CircleAvatar(
-                  radius: 25,
-                  backgroundColor: Colors.white,
-                  child: IconButton(
-                      onPressed: null,
-                      icon: Icon(
-                        Icons.add_task,
-                        color: Colors.black,
-                      ))),
-              label: '',
-              activeIcon: CircleAvatar(
-                  radius: 25,
-                  backgroundColor: Colors.blue,
-                  child: IconButton(
-                      onPressed: null,
-                      icon: Icon(
-                        Icons.add_task,
-                        color: Colors.white,
-                      )))),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        selectedItemColor: Colors.white,
-        // backgroundColor: Colors.white,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-      ),
-    );
+        body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+        bottomNavigationBar: Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(10), topLeft: Radius.circular(10)),
+            boxShadow: [
+              BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.25),
+                  spreadRadius: 0,
+                  blurRadius: 4),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10.0),
+                topRight: Radius.circular(10.0)),
+            child: BottomNavigationBar(
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              unselectedFontSize: 0.0,
+              selectedFontSize: 0.0,
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                    label: "",
+                    icon: const CircleAvatar(
+                        radius: 25,
+                        backgroundColor: Colors.white,
+                        child: IconButton(
+                            onPressed: null,
+                            icon: Icon(
+                              Icons.home,
+                              color: Colors.black,
+                            ))).paddingAll(10),
+                    activeIcon: const CircleAvatar(
+                        radius: 25,
+                        backgroundColor: Color.fromRGBO(139, 208, 252, 1),
+                        child: IconButton(
+                            onPressed: null,
+                            icon: Icon(
+                              Icons.home,
+                              color: Colors.white,
+                            ))).paddingAll(10)),
+                const BottomNavigationBarItem(
+                    icon: CircleAvatar(
+                        radius: 25,
+                        backgroundColor: Colors.white,
+                        child: IconButton(
+                            onPressed: null,
+                            icon: Icon(
+                              Icons.map,
+                              color: Colors.black,
+                            ))),
+                    label: '',
+                    activeIcon: CircleAvatar(
+                        radius: 25,
+                        backgroundColor: Color.fromRGBO(139, 208, 252, 1),
+                        child: IconButton(
+                            onPressed: null,
+                            icon: Icon(
+                              Icons.map,
+                              color: Colors.white,
+                            )))),
+                const BottomNavigationBarItem(
+                    icon: CircleAvatar(
+                        radius: 25,
+                        backgroundColor: Colors.white,
+                        child: IconButton(
+                            onPressed: null,
+                            icon: Icon(
+                              Icons.location_searching,
+                              color: Colors.black,
+                            ))),
+                    label: '',
+                    activeIcon: CircleAvatar(
+                        radius: 25,
+                        backgroundColor: Color.fromRGBO(139, 208, 252, 1),
+                        child: IconButton(
+                            onPressed: null,
+                            icon: Icon(
+                              Icons.location_searching,
+                              color: Colors.white,
+                            )))),
+                const BottomNavigationBarItem(
+                    icon: CircleAvatar(
+                        radius: 25,
+                        backgroundColor: Colors.white,
+                        child: IconButton(
+                            onPressed: null,
+                            icon: Icon(
+                              Icons.add_task,
+                              color: Colors.black,
+                            ))),
+                    label: '',
+                    activeIcon: CircleAvatar(
+                        radius: 25,
+                        backgroundColor: Color.fromRGBO(139, 208, 252, 1),
+                        child: IconButton(
+                            onPressed: null,
+                            icon: Icon(
+                              Icons.add_task,
+                              color: Colors.white,
+                            )))),
+              ],
+              currentIndex: _selectedIndex,
+              onTap: _onItemTapped,
+              selectedItemColor: Colors.white,
+            ),
+          ),
+        ));
   }
 
   @override
